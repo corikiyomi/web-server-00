539414994bb116039d7a1b82ee35c61136340b94
Cori Komiyama <corikiyomi@gmail.com>
2024-02-29 10:30:05 -0700

    Issue: Live server showing page with folder and not html page.

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 print(__name__)
 
 @app.route('/') # Decorator
 def hello_world():
-    return '<p>Hello, World!</p>'
+    return render_template('index.html')
+
+@app.route('/about.html')
+def about():
+    return 'About me'
