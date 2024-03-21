fc01b80f344976d9aa768cbc72adb8d127b563a3
Cori Komiyama <corikiyomi@gmail.com>
2024-03-21 16:14:36 -0700

    Bug: HTTP Error 405 when submitting form

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 print(__name__)
 
@@ -9,3 +10,7 @@ def my_home():
 @app.route('/<string:page_name>') # Dynamic API endpoints to eliminate repetitive code
 def html_page(page_name):
     return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    return 'Form submitted successfully.'
