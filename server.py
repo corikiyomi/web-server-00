a19be706d68e52508122e9bbc1af735fcc503487
Cori Komiyama <corikiyomi@gmail.com>
2024-03-21 15:58:32 -0700

    Updated API endpoints to be more dynamic

--- server.py
+++ server.py
@@ -2,18 +2,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 print(__name__)
 
-@app.route('/') # Decorator
+@app.route('/') # Decorator @
 def my_home():
     return render_template('index.html')
 
-@app.route('/about') # API Endpoint determines what to send to the front end to display the appropriate information
-def about_me():
-    return render_template('about.html')
-
-@app.route('/contact')
-def contact_me():
-    return render_template('contact.html')
-
-@app.route('/works')
-def my_works():
-    return render_template('works.html')
+@app.route('/<string:page_name>') # Dynamic API endpoints to eliminate repetitive code
+def html_page(page_name):
+    return render_template(page_name)
