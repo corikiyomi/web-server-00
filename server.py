528fd9030936735c6d61927812910c7b17346020
Cori Komiyama <corikiyomi@gmail.com>
2024-03-21 15:43:14 -0700

    Updated hrefs for restructure. Edited API endpoints

--- server.py
+++ server.py
@@ -2,14 +2,18 @@ from flask import Flask, render_template
 app = Flask(__name__)
 print(__name__)
 
-@app.route('/<username>/<int:post_id>') # Decorator
-def hello_world(username=None, post_id=None): # Set the default 
-    return render_template('index.html', name=username, post_id=post_id)
+@app.route('/') # Decorator
+def my_home():
+    return render_template('index.html')
 
-@app.route('/blog') # API Endpoint determines what to send to the front end to display the appropriate information
-def blog():
-    return 'These are my thoughts on blogs'
-
-@app.route('/about')
-def about():
+@app.route('/about') # API Endpoint determines what to send to the front end to display the appropriate information
+def about_me():
     return render_template('about.html')
+
+@app.route('/contact')
+def contact_me():
+    return render_template('contact.html')
+
+@app.route('/works')
+def my_works():
+    return render_template('works.html')
