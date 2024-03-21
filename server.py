6257d00d0ef6cd5d5c3b3bbdb08e6a292259651e
Cori Komiyama <corikiyomi@gmail.com>
2024-03-21 13:10:56 -0700

    Update html

--- server.py
+++ server.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 print(__name__)
 
-@app.route('/') # Decorator
-def hello_world():
-    return render_template('index.html')
+@app.route('/<username>/<int:post_id>') # Decorator
+def hello_world(username=None, post_id=None): # Set the default 
+    return render_template('index.html', name=username, post_id=post_id)
 
-@app.route('/blog')
+@app.route('/blog') # URL Endpoint determines what to send to the front end to display the appropriate information
 def blog():
     return 'These are my thoughts on blogs'
 
